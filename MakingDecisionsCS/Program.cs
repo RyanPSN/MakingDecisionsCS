c31424cc180e3729e721f467ab8861dbca6c1532
RyanPSN <ryangregoire286@gmail.com>
2024-10-02 17:58:51 -0400

    Add project files.

---
+++ MakingDecisionsCS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MakingDecisionsCS
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            Console.WriteLine("Enter Your Current Age: ");
+            int a = int.Parse(Console.ReadLine());
+            bool ageAge = (a >= 18) && (a <= 35);
+           
+            if (ageAge)
+            {
+                Console.WriteLine("Older Than 18: {0} ", ageAge);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Younger than 18: {0}", ageAge);
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
