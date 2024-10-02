699d9f6f05b882ec8a38ddf2347410b8ef4e7226
RyanPSN <ryangregoire286@gmail.com>
2024-10-02 17:58:51 -0400

    Added If CheckAdd project files.

---
+++ MakingDecisionsCS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MakingDecisionsCS
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            Console.WriteLine("Welcome to Age Application");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter Your Current Age: ");
+            int a = int.Parse(Console.ReadLine());
+            bool ageAgeOlder = (a >= 18) && (a <= 35);
+            bool ageAgeYounger = (a < 18) && (a <= 35);
+           
+            if (ageAgeOlder)
+            {
+                Console.WriteLine("Older Than 18: {0} ", ageAgeOlder);
+                Console.WriteLine("Younger than 18: {0}", ageAgeYounger);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Younger than 18: {0}", ageAgeYounger);
+                Console.WriteLine("Older than 18: {0}", ageAgeOlder);
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
