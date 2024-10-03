c32ab6fe6c5290514a9e1063ac523a88b918cfd4
RyanPSN <ryangregoire286@gmail.com>
2024-10-03 10:29:42 -0400

    ADDED

--- MakingDecisionsCS/Program.cs
+++ MakingDecisionsCS/Program.cs
@@ -14,22 +14,31 @@ namespace MakingDecisionsCS
             int a = int.Parse(Console.ReadLine());
             bool ageAgeOlder = (a >= 18) && (a <= 35);
             bool ageAgeYounger = (a < 18) && (a <= 35);
-           
+
             if (ageAgeOlder)
             {
                 Console.WriteLine("Older Than 18: {0} ", ageAgeOlder);
                 Console.WriteLine("Younger than 18: {0}", ageAgeYounger);
-                Console.ReadLine();
+                bool ageAge = (a >= 18) && (a <= 35);
+
+                if (ageAge)
+                {
+                    Console.WriteLine("Older Than 18: {0} ", ageAge);
+
+                    Console.ReadLine();
+                }
+                else
+                {
+
+                    Console.WriteLine("Younger than 18: {0}", ageAgeYounger);
+                    Console.WriteLine("Older than 18: {0}", ageAgeOlder);
+
+                    Console.WriteLine("Younger than 18: {0}", ageAge);
+                    Console.ReadLine();
+                }
+
+
             }
-            else
-            {
-                Console.WriteLine("Younger than 18: {0}", ageAgeYounger);
-                Console.WriteLine("Older than 18: {0}", ageAgeOlder);
-                Console.ReadLine();
-            }
-
-
-
         }
     }
 }
